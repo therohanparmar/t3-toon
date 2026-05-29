a842351add66dcea51a4675e6f2cb6142bbf4e9c
Rohan Parmar <72159621+therohanparmar@users.noreply.github.com>
2026-05-29 14:07:30 +0530

    Update TYPO3 version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF['rrp_t3toon'] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-14.9.99',
+            'typo3' => '12.4.0-14.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
